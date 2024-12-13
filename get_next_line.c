0239bd64c5be9ca2923307b813b7a327474223ea
Ayoub Laaoufi <aylaaouf@c3r6p1.1337.ma>
2024-12-13 08:32:01 +0100

    gnl

---
+++ get_next_line.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 02:46:59 by aylaaouf          #+#    #+#             */
+/*   Updated: 2024/12/13 07:16:12 by aylaaouf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char    *read_buffer(int fd, char *buffer)
+{
+    char    *temp_buffer;
+    int     bytes_read;
+
+    temp_buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    bytes_read = read(fd, temp_buffer, BUFFER_SIZE);
+    if (bytes_read < 0)
+    {
+        free(temp_buffer);
+        return (NULL);
+    }
+    temp_buffer[bytes_read] = '\0';
+    if (bytes_read == 0)
+    {
+        free(temp_buffer);
+        return (buffer);
+    }
+    buffer = ft_strjoin(buffer, temp_buffer);
+    free(temp_buffer);
+    return (buffer);
+}
+
+char    *get_line(char *buffer)
+{
+    char    *line;
+    char    *new_line_pos;
+    size_t  len;
+    int     i;
+
+    new_line_pos = ft_strchr(buffer, '\n');
+    if (new_line_pos)
+        len = new_line_pos - buffer;
+    else
+        len = ft_strlen(buffer);
+    line = malloc(sizeof(char) * (len + 1));
+    if (!line)
+        return (NULL);
+    i = 0;
+    while (i < len)
+        line[i++] = buffer[i++];
+    line[len] = '\0';
+    return (line);
+}
+
+char    *get_next_line(int fd)
+{
+    static char *buffer;
+    char        *line;
+    
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+        
+    buffer = read_buffer(fd, buffer);
+    if (!buffer)
+        return (NULL);
+    line = get_line(buffer);
+    if (!buffer)
+        return (NULL);
+    buffer = update_buffer(buffer);
+    return (line);
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/12 19:23:40 by aylaaouf          #+#    #+#             */
+/*   Updated: 2024/12/13 06:21:05 by aylaaouf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if ((unsigned char)s[i] == (unsigned char)c)
+		{
+			return ((char *)(&s[i]));
+		}
+		i++;
+	}
+	if ((unsigned char)s[i] == (unsigned char)c)
+		return ((char *)(&s[i]));
+	return (NULL);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*ptr;
+	int		len;
+
+	len = ft_strlen(s1);
+	ptr = (char *)malloc(len + 1);
+	if (!ptr)
+		return (NULL);
+	ft_memcpy(ptr, s1, len);
+	ptr[len] = '\0';
+	return (ptr);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	size_t	len;
+	char	*str;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = 0;
+	j = 0;
+	len = ft_strlen(s1) + ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	while (s1[i])
+	{
+		str[j++] = s1[i++];
+	}
+	i = 0;
+	while (s2[i])
+	{
+		str[j++] = s2[i++];
+	}
+	str[j] = '\0';
+	return (str);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
